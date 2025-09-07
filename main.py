01266408d613245937bd04a398cdcac7a8eff103
MamreyAisultan <mamreyaisultan123@gmail.com>
2025-09-07 12:23:12 +0500

    feat: Added new methods to all classes.

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ class Animal:
     def make_sound(self):
         print("Animal makes some sound...")
 
+    def feed(self):
+        print(f"{self.name} Eats")
+
 animal = Animal("Безимянное животное", "3")
 print(f"{animal.name}, {animal.age} лет")
 animal.make_sound()
@@ -14,13 +17,22 @@ class Lion(Animal):
     def make_sound(self):
         print("Roar!")
 
+    def feed(self):
+        print(f"{self.name} Eats meat")
+
 class Monkey(Animal):
     def make_sound(self):
         print("Ooh, Ooh!")
 
+    def feed(self):
+        print(f"{self.name} Eats banana")
+
 class Elephant(Animal):
     def make_sound(self):
         print("Trumpet!")
+
+    def feed(self):
+        print(f"{self.name} Eats grass")
 
 lion = Lion("Yerrasyl", 18)
 monkey = Monkey("Aknazar", 15)
@@ -29,3 +41,7 @@ elephant = Elephant("Persidskiy", 15)
 lion.make_sound()
 monkey.make_sound()
 elephant.make_sound()
+
+lion.feed()
+monkey.feed()
+elephant.feed()
