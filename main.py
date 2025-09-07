8ade7a4baa0f5a4fedf2ffee6cc5e5789f3f70e7
MamreyAisultan <mamreyaisultan123@gmail.com>
2025-09-07 12:10:09 +0500

    feat: Added animal sounds

--- main.py
+++ main.py
@@ -9,3 +9,23 @@ class Animal:
 animal = Animal("Безимянное животное", "3")
 print(f"{animal.name}, {animal.age} лет")
 animal.make_sound()
+
+class Lion(Animal):
+    def make_sound(self):
+        print("Roar!")
+
+class Monkey(Animal):
+    def make_sound(self):
+        print("Ooh, Ooh!")
+
+class Elephant(Animal):
+    def make_sound(self):
+        print("Trumpet!")
+
+lion = Lion("Yerrasyl", 18)
+monkey = Monkey("Aknazar", 15)
+elephant = Elephant("Persidskiy", 15)
+
+lion.make_sound()
+monkey.make_sound()
+elephant.make_sound()
